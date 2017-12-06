a5f98ecfdbe462ff214cf49f8bb52e16bd4e6bde
NickGatti <nick.gatti@gmail.com>
2017-12-06 12:01:40 -0700

    All Books Title and Authors

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ $( document ).ready( function () {
                 let which = 'bookNum' + i
                 $( '.results' ).append( `<div class="book" id="${which}"></div>` )
                 for ( let key in data[ i ] ) {
-                    $( "#" + which ).append( `<p>${data[i][key]}</p>` )
+                    if ( key === 'title' ) {
+                        $( "#" + which ).append( `<p>Title: ${data[i][key]}</p>` )
+                    } else if ( key === 'author_id' ) {
+                        let passArg = data[ i ][ key ]
+                        findAuthors( passArg, which )
+                    }
+
                 }
             }
         } )
@@ -20,14 +26,18 @@ $( document ).ready( function () {
                 $( '.results' ).append( `<div class="book" id="${which}"></div>` )
                 for ( let key in data[ i ] ) {
                     if ( key === "author_id" ) {
-                        $.get( 'https://tmartin-books-api.herokuapp.com/authors/' + data[ i ][ key ], function ( author ) {
-                            for ( let z = 0; z < author.length; z++ ) {
-                                $( "#" + which ).append( `<p>${author[z].first_name} ${author[z].last_name}</p>` )
-                            }
-                        } )
+                        let passArg = data[ i ][ key ]
+                        findAuthors( passArg, which )
                     }
-
                 }
+            }
+        } )
+    }
+
+    function findAuthors( data, which ) {
+        $.get( 'https://tmartin-books-api.herokuapp.com/authors/' + data, function ( author ) {
+            for ( let z = 0; z < author.length; z++ ) {
+                $( "#" + which ).append( `<p>${author[z].first_name} ${author[z].last_name}</p>` )
             }
         } )
     }
