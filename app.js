b525c5452627f4e0ee29e06741a3c230aff476ad
NickGatti <nick.gatti@gmail.com>
2017-12-06 13:42:39 -0700

    refactor

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 $( document ).ready( function () {
-    function getBooks( e ) {
-        e.preventDefault()
+
+    function mainToName( keyComparison ) {
         $.get( 'https://tmartin-books-api.herokuapp.com/books', function ( data ) {
+            $( '.results' ).empty()
             for ( let i = 0; i < data.length; i++ ) {
                 let which = 'bookNum' + i
                 $( '.results' ).append( `<div class="book" id="${which}"></div>` )
                 for ( let key in data[ i ] ) {
-                    if ( key === 'title' ) {
-                        $( "#" + which ).append( `<p>Title: ${data[i][key]}</p>` )
-                    } else if ( key === 'author_id' ) {
-                        let passArg = data[ i ][ key ]
-                        findAuthors( passArg, which )
+                    for ( let x = 0; x < keyComparison.length; x++ ) {
+                        if ( key === keyComparison[ x ] ) {
+                            if ( key === 'author_id' ) {
+                                let passArg = data[ i ][ key ]
+                                findAuthors( passArg, which )
+                            } else {
+                                $( "#" + which ).append( `<p>Title: ${data[i][key]}</p>` )
+                            }
+                        }
                     }
-
                 }
             }
         } )
@@ -21,6 +25,7 @@ $( document ).ready( function () {
     function getAuthors( e ) {
         e.preventDefault()
         $.get( 'https://tmartin-books-api.herokuapp.com/books', function ( data ) {
+            $( '.results' ).empty()
             for ( let i = 0; i < data.length; i++ ) {
                 let which = 'bookNum' + i
                 $( '.results' ).append( `<div class="book" id="${which}"></div>` )
@@ -34,6 +39,32 @@ $( document ).ready( function () {
         } )
     }
 
+    function getGenre( e ) {
+        e.preventDefault()
+        $.get( 'https://tmartin-books-api.herokuapp.com/genres', function ( data ) {
+            for ( let i = 0; i < data.length; i++ ) {
+                if ( data[ i ].genre === $( '#search' ).val() ) {
+                    $.get( 'https://tmartin-books-api.herokuapp.com/genre/' + $( '#search' ).val(), function ( data ) {
+                        $( '.results' ).empty()
+                        for ( let i = 0; i < data.length; i++ ) {
+                            let which = 'bookNum' + i
+                            $( '.results' ).append( `<div class="book" id="${which}"></div>` )
+                            for ( let key in data[ i ] ) {
+                                if ( key === 'title' ) {
+                                    $( "#" + which ).append( `<p>Title: ${data[i][key]}</p>` )
+                                } else if ( key === 'author_id' ) {
+                                    let passArg = data[ i ][ key ]
+                                    findAuthors( passArg, which )
+                                }
+                            }
+                        }
+                    } )
+                    break;
+                }
+            }
+        } )
+    }
+
     function findAuthors( data, which ) {
         $.get( 'https://tmartin-books-api.herokuapp.com/authors/' + data, function ( author ) {
             for ( let z = 0; z < author.length; z++ ) {
@@ -41,6 +72,13 @@ $( document ).ready( function () {
             }
         } )
     }
-    $( '#getBooks' ).click( getBooks )
-    $( '#getAuthors' ).click( getAuthors )
+    $( '#getBooks' ).click( function ( e ) {
+        e.preventDefault()
+        mainToName( [ 'title', 'author_id' ] )
+    } )
+    $( '#getAuthors' ).click( function ( e ) {
+        e.preventDefault()
+        mainToName( [ 'author_id' ] )
+    } )
+    $( '#findBooks' ).click( getGenre )
 } )
