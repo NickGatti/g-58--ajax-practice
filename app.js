8352ef8bcedaf18d76918e70862515033dd60c73
NickGatti <nick.gatti@gmail.com>
2017-12-06 11:39:43 -0700

    getBooks

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-( function () {
-    function getBooks( e ) {}
-
+$( document ).ready( function () {
+    function getBooks( e ) {
+        e.preventDefault()
+        $.get( 'https://tmartin-books-api.herokuapp.com/books', function ( data ) {
+            for ( let i = 0; i < data.length; i++ ) {
+                let which = 'bookNum' + i
+                $( '.results' ).append( `<div class="book" id="${which}"></div>` )
+                for ( let key in data[ i ] ) {
+                    $( "#" + which ).append( `<p>${data[i][key]}</p>` )
+                }
+            }
+        } )
+    }
     $( '#getBooks' ).click( getBooks )
-} )();
+    $( '#getAuthors' ).click( getAuthors )
+} )
