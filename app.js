e1d54867df5046cec48952e04b019c6e06357063
NickGatti <nick.gatti@gmail.com>
2017-12-06 14:07:23 -0700

    Genres

--- app.js
+++ app.js
@@ -7,15 +7,19 @@ $( document ).ready( function () {
                 let which = 'bookNum' + i
                 $( '.results' ).append( `<div class="book" id="${which}"></div>` )
                 for ( let key in data[ i ] ) {
-                    for ( let x = 0; x < keyComparison.length; x++ ) {
-                        if ( key === keyComparison[ x ] ) {
-                            if ( key === 'author_id' ) {
-                                let passArg = data[ i ][ key ]
-                                findAuthors( passArg, which )
-                            } else {
-                                $( "#" + which ).append( `<p>Title: ${data[i][key]}</p>` )
+                    if ( Array.isArray( keyComparison ) ) {
+                        for ( let x = 0; x < keyComparison.length; x++ ) {
+                            if ( key === keyComparison[ x ] ) {
+                                if ( key === 'author_id' ) {
+                                    let passArg = data[ i ][ key ]
+                                    findAuthors( passArg, which )
+                                } else {
+                                    $( "#" + which ).append( `<p>Title: ${data[i][key]}</p>` )
+                                }
                             }
                         }
+                    } else if ( keyComparison.id === data[ i ].genre_id ) {
+                        $( "#" + which ).append( `<p>${data[i][key]}</p>` )
                     }
                 }
             }
@@ -27,20 +31,9 @@ $( document ).ready( function () {
         $.get( 'https://tmartin-books-api.herokuapp.com/genres', function ( data ) {
             for ( let i = 0; i < data.length; i++ ) {
                 if ( data[ i ].genre === $( '#search' ).val() ) {
-                    $.get( 'https://tmartin-books-api.herokuapp.com/genre/' + $( '#search' ).val(), function ( data ) {
-                        $( '.results' ).empty()
-                        for ( let i = 0; i < data.length; i++ ) {
-                            let which = 'bookNum' + i
-                            $( '.results' ).append( `<div class="book" id="${which}"></div>` )
-                            for ( let key in data[ i ] ) {
-                                if ( key === 'title' ) {
-                                    $( "#" + which ).append( `<p>Title: ${data[i][key]}</p>` )
-                                } else if ( key === 'author_id' ) {
-                                    let passArg = data[ i ][ key ]
-                                    findAuthors( passArg, which )
-                                }
-                            }
-                        }
+                    mainToName( {
+                        'genre': $( '#search' ).val(),
+                        'id': data[ i ].id
                     } )
                     break;
                 }
